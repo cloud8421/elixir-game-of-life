73f54e6d75cd73acad96071ae8112e5659f2f436
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-10-27 16:00:51 +0000

    Adds instructions.

--- lib/grid.ex
+++ lib/grid.ex
@@ -1,6 +1,11 @@
 defmodule Grid do
   use GenServer
 
+  # Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+  # Any live cell with two or three live neighbours lives on to the next generation.
+  # Any live cell with more than three live neighbours dies, as if by overcrowding.
+  # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
   def cells do
     GenServer.call(__MODULE__, :cells)
   end
