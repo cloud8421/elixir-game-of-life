5608dba1d54ad5185e89ad2c2b01b747a72bd27c
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-11-05 09:21:02 -0600

    Abstract 0/1 map to :dead/:alive.

--- lib/cell.ex
+++ lib/cell.ex
@@ -1,10 +1,10 @@
 defmodule Cell do
   defstruct x: nil, y: nil, status: nil
 
-  def from_initial_value({0, x, y}) do
-    %Cell{x: x, y: y, status: :dead}
+  def from_initial_value({status_int, x, y}) do
+    %Cell{x: x, y: y, status: status_for(status_int)}
   end
-  def from_initial_value({1, x, y}) do
-    %Cell{x: x, y: y, status: :alive}
-  end
+
+  defp status_for(1), do: :alive
+  defp status_for(0), do: :dead
 end
