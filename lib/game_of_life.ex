63f3397b072331efd5525e43c87be4ac5faec8d3
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-11-05 09:21:30 -0600

    Remove noise.

--- lib/game_of_life.ex
+++ lib/game_of_life.ex
@@ -1,8 +1,6 @@
 defmodule GameOfLife do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
@@ -17,12 +15,8 @@ defmodule GameOfLife do
 
     children = [
       worker(GridWorker, [initial_layout])
-      # Define workers and child supervisors to be supervised
-      # worker(GameOfLife.Worker, [arg1, arg2, arg3])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: GameOfLife.Supervisor]
     Supervisor.start_link(children, opts)
   end
